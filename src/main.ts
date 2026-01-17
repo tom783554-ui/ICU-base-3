4cfc259420ff442e4bc25e4791ec65e819714f28
tom783554-ui <tom783554@gmail.com>
2026-01-16 19:31:06 -0600

    Initialize Vite TypeScript app

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div class="app">
+    <h1>Vite + TypeScript</h1>
+    <p class="read-the-docs">
+      Edit <code>src/main.ts</code> and save to test HMR
+    </p>
+  </div>
+`
